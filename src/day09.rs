d844e91d09f2d2ec5487644f2d5242306bf0075c
Brennan Tracy <brennantracy4@gmail.com>
2022-06-27 20:15:12 -0700

    idk doing stuff in france

--- src/day09.rs
+++ src/day09.rs
@@ -3,13 +3,14 @@ use std::collections::{HashSet, VecDeque};
 use super::AdventOfCode2021;
 use crate::aoc::ParseInput;
 use crate::aoc::{Day, Part, Solution};
+use crate::point2d::Point2D;
 
 impl ParseInput<'_, { Day::Nine }> for AdventOfCode2021<{ Day::Nine }> {
     type Parsed = Vec<Vec<u32>>;
 
     fn parse_input(&self, input: &'_ str) -> Self::Parsed {
         input
-            .split('\n')
+            .lines()
             .map(|line| {
                 line.chars()
                     .map(|digit| digit.to_digit(10).expect("Expected Digit"))
@@ -19,51 +20,53 @@ impl ParseInput<'_, { Day::Nine }> for AdventOfCode2021<{ Day::Nine }> {
     }
 }
 
-fn get_low_points(height_map: &[Vec<u32>]) -> Vec<(usize, usize)> {
+fn get_low_points(height_map: &[Vec<u32>]) -> Vec<Point2D<usize>> {
     let mut ret = Vec::new();
     let num_rows = height_map.len() as i32;
     let num_col = height_map[0].len() as i32;
     for i in 0..num_rows {
         for j in 0..num_col {
             let neighbors = get_neighbors(i, j, num_rows, num_col);
+            let x = i as usize;
+            let y = j as usize;
             if neighbors
                 .iter()
-                .all(|(x, y)| height_map[*x][*y] > height_map[i as usize][j as usize])
+                .all(|other| height_map[other.x][other.y] > height_map[x][y])
             {
-                ret.push((i as usize, j as usize));
+                ret.push(Point2D {x, y});
             }
         }
     }
     ret
 }
 
-fn get_neighbors(i: i32, j: i32, num_rows: i32, num_col: i32) -> Vec<(usize, usize)> {
+fn get_neighbors(i: i32, j: i32, num_rows: i32, num_col: i32) -> Vec<Point2D<usize>> {
     let mut neighbors = Vec::with_capacity(4);
     for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
         let new_x = i + dx;
         let new_y = j + dy;
         if new_x >= 0 && new_x < num_rows && new_y >= 0 && new_y < num_col {
-            neighbors.push((new_x as usize, new_y as usize))
+            neighbors.push(Point2D{x: new_x as usize, y: new_y as usize})
         }
     }
     neighbors
 }
 
-fn calculate_basin_size(height_map: &[Vec<u32>], low_point: (usize, usize)) -> u32 {
-    let mut frontier: VecDeque<(usize, usize)> = VecDeque::from([low_point]);
-    let mut visited: HashSet<(usize, usize)> = HashSet::from([low_point]);
+fn calculate_basin_size(height_map: &[Vec<u32>], low_point: Point2D<usize>) -> u32 {
+    let mut frontier: VecDeque<Point2D<usize>> = VecDeque::from([low_point]);
+    let mut visited: HashSet<Point2D<usize>> = HashSet::from([low_point]);
     let mut ret = 0;
     while !frontier.is_empty() {
         let node = frontier.pop_front().unwrap();
         ret += 1;
         for neighbor in get_neighbors(
-            node.0 as i32,
-            node.1 as i32,
+            node.x as i32,
+            node.y as i32,
             height_map.len() as i32,
             height_map[0].len() as i32,
         ) {
-            if height_map[neighbor.0][neighbor.1] > height_map[node.0][node.1]
-                && height_map[neighbor.0][neighbor.1] != 9
+            if height_map[neighbor.x][neighbor.y] > height_map[node.x][node.y]
+                && height_map[neighbor.x][neighbor.y] != 9
                 && !visited.contains(&neighbor)
             {
                 visited.insert(neighbor);
@@ -81,7 +84,7 @@ impl Solution<'_, { Day::Nine }, { Part::One }> for AdventOfCode2021<{ Day::Nine
     fn solve(&self, input: &Self::Input) -> Self::Output {
         get_low_points(input)
             .iter()
-            .map(|(x, y)| input[*x][*y] + 1)
+            .map(|point| input[point.x][point.y] + 1)
             .sum()
     }
 }
@@ -93,7 +96,7 @@ impl Solution<'_, { Day::Nine }, { Part::Two }> for AdventOfCode2021<{ Day::Nine
     fn solve(&self, input: &Self::Input) -> Self::Output {
         let mut basin_sizes: Vec<u32> = get_low_points(input)
             .iter()
-            .filter(|(x, y)| input[*x][*y] != 9)
+            .filter(|point| input[point.x][point.y] != 9)
             .map(|low_point| calculate_basin_size(input, *low_point))
             .collect();
         basin_sizes.sort_unstable();
--- src/point2d.rs
+++ src/point2d.rs
@@ -1,5 +1,21 @@
+use std::str::FromStr;
+use std::fmt::Debug;
+
 #[derive(Copy, Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Point2D<T> {
     pub x: T,
     pub y: T,
+}
+
+impl<T> FromStr for Point2D<T> 
+where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::fmt::Debug
+{
+    type Err = std::string::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s.split_once(',').expect("Failed to find comma");
+        Ok(Point2D {
+            x: split.0.parse().expect("Failed to parse coordinate"),
+            y: split.1.parse().expect("Failed to parse coordinate"),
+        })
+    }
 }
