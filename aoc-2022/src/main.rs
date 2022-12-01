816cf3f3808a1eded61b556781880fcf890bcdb8
Brennan Tracy <brennantracy4@gmail.com>
2022-11-30 21:52:25 -0800

    Begin aoc-2022 baby

---
+++ aoc-2022/src/main.rs
@@ -0,0 +1,35 @@
+#![allow(incomplete_features)]
+#![feature(adt_const_params)]
+#![feature(iter_advance_by)]
+#![feature(specialization)]
+#![feature(map_first_last)]
+
+mod day01;
+
+use anyhow::anyhow;
+use anyhow::Context;
+use anyhow::Result;
+use aoc_runner::run_solutions;
+use aoc_runner::Day;
+use aoc_runner::SolutionRunner;
+use std::fs::read_to_string;
+
+pub struct AOC2022<const DAY: Day>;
+
+fn input(day: &Day) -> Result<String> {
+    let day: u8 = (*day).into();
+    let path = format!("aoc-2022/input/day{}.txt", day);
+    read_to_string(&path).context(path)
+}
+
+fn solve(day: &Day) -> Result<()> {
+    let input = input(day)?;
+    match day {
+        Day::Day1 => AOC2022::<{ Day::Day1 }>.run(&input),
+        _ => Err(anyhow!("Day not implemented yet")),
+    }
+}
+
+fn main() {
+    run_solutions(&solve);
+}
