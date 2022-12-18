6c532b6a253d16faa8c95ed58060f62eb63db799
Brennan Tracy <brennantracy4@gmail.com>
2022-12-17 16:09:04 -0800

    2022 Day7 continue

--- aoc-2022/src/day07.rs
+++ aoc-2022/src/day07.rs
@@ -5,50 +5,62 @@ use aoc_runner::{Day, ParseInput, Part, Solution};
 
 use anyhow::Context;
 use anyhow::Result;
+use camino::Utf8Path;
+use nom::branch::alt;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take_while;
+use nom::combinator::map;
 use nom::multi::separated_list0;
 use nom::IResult;
 
-enum Command {
-    LS(String),
-    CD(String),
+fn parse_path(s: &str) -> IResult<&str, Utf8Path> {
+    map(
+        take_while(|c: char| c.is_alphanumeric() || c == '.' || c == '-')(s)?,
+        Utf8Path::from_str,
+    )
 }
 
-impl FromStr for Command {
-    type Err = nom::Err<(String, nom::error::ErrorKind)>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
-    }
+#[derive(Debug)]
+enum Binary {
+    LS,
+    CD,
 }
 
+fn parse_binary(s: &str) -> IResult<&str, Binary> {
+    alt((
+        map(tag("ls"), |_| Binary::LS),
+        map(tag("cd"), |_| Binary::CD),
+    ))(s)
+}
+
+#[derive(Debug)]
 enum Entry {
-    Dir(String),
-    File(String, usize),
+    Dir(Utf8Path),
+    File(Utf8Path, usize),
 }
 
+fn parse_entry(s: &str) -> IResult<&str, Entry> {
+    alt((parse_dir, parse_file))(s)
+}
+
+#[derive(Debug)]
 enum TerminalOutput {
-    Command(Command),
+    Command(Binary, Utf8Path),
     FileEntry(Entry),
 }
 
-fn command(s: &str) -> IResult<&str, Command> {
-    tuple((tag("$ "), command_name, tag(" "), command_arg))(s)
-        .map(|(i, (_, name, _, arg))| (i, Command::new(name, arg)))
-}
-
-fn terminal_line(s: &str) -> IResult<&str, TerminalOutput> {
+fn parse_terminal_line(s: &str) -> IResult<&str, TerminalOutput> {
     alt((command, file_entry))(s)
 }
 
-fn terminal_output(s: &str) -> IResult<&str, Vec<TerminalOutput>> {
+fn parse_terminal_output(s: &str) -> IResult<&str, Vec<TerminalOutput>> {
     separated_list0(tag("\n"), terminal_line)(s)
 }
 
 impl ParseInput<'_, { Day::Day7 }> for AOC2022<{ Day::Day7 }> {
     type Parsed = Vec<TerminalOutput>;
 
-    fn parse_input(&self, input: &'_ str) -> Result<Self::Parsed> {
+    fn parse_inpu(&self, input: &'_ str) -> Result<Self::Parsed> {
         let (_, terminal_output) = terminal_output(input)?;
         Ok(terminal_output)
     }
