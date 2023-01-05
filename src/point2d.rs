a0b668c1a2df90a4521c29ae65c9bba06315a98d
Brennan Tracy <brennantracy4@gmail.com>
2023-01-04 16:38:16 -0800

    2022 Day9 why is this so hard?

--- src/point2d.rs
+++ src/point2d.rs
@@ -1,7 +1,8 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display, Formatter};
+use std::ops::{Add, Sub};
 use std::str::FromStr;
 
-#[derive(Copy, Clone, Debug, Default, Eq, Hash, PartialEq)]
+#[derive(Copy, Clone, Default, Eq, Hash, PartialEq)]
 pub struct Point2D<T> {
     pub x: T,
     pub y: T,
@@ -25,5 +26,43 @@ where
             x: split.0.parse().expect("Failed to parse coordinate"),
             y: split.1.parse().expect("Failed to parse coordinate"),
         })
+    }
+}
+
+impl<T> Display for Point2D<T> where T: Display {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
+impl<T> Debug for Point2D<T> where T: Debug {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({:?},{:?})", self.x, self.y)
+    }
+}
+
+impl<T> Add for Point2D<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl<T> Sub for Point2D<T>
+where
+    T: Sub<Output = T>,
+{
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
     }
 }
