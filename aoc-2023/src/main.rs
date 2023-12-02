ddd4b55c3b3ddf6221bc1b85e47e0c269487283b
Brennan Tracy <brennantracy4@gmail.com>
2023-12-01 23:25:30 -0800

    Aoc 2023 day 1 complete.

---
+++ aoc-2023/src/main.rs
@@ -0,0 +1,34 @@
+#![allow(incomplete_features)]
+#![feature(adt_const_params)]
+#![feature(iter_advance_by)]
+#![feature(specialization)]
+
+mod day01;
+
+use anyhow::anyhow;
+use anyhow::Context;
+use anyhow::Result;
+use aoc_runner::run_solutions;
+use aoc_runner::Day;
+use aoc_runner::SolutionRunner;
+use std::fs::read_to_string;
+
+pub struct AOC2023<const DAY: Day>;
+
+fn input(day: &Day) -> Result<String> {
+    let day: u8 = (*day).into();
+    let path = format!("aoc-2023/input/day{day}.txt");
+    read_to_string(&path).context(path)
+}
+
+fn solve(day: &Day) -> Result<()> {
+    let input = input(day)?;
+    match day {
+        Day::Day1 => AOC2023::<{ Day::Day1 }>.run(&input),
+        _ => Err(anyhow!("Day not implemented yet")),
+    }
+}
+
+fn main() {
+    run_solutions(&solve);
+}
