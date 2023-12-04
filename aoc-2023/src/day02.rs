987799842657cb56678f41f63c7b511383ad3e63
Brennan Tracy <brennantracy4@gmail.com>
2023-12-03 20:36:29 -0800

    Aoc 2022 day 2 complete.

--- aoc-2023/src/day02.rs
+++ aoc-2023/src/day02.rs
@@ -6,12 +6,14 @@ use nom::{
     bytes::complete::tag,
     character::complete::{digit1, space1},
     combinator::{all_consuming, map, map_res, recognize},
-    multi::{many1, separated_list1},
+    multi::separated_list1,
     sequence::{preceded, separated_pair},
     IResult,
 };
+use std::cmp::max;
 
-enum Color {
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum Color {
     Red,
     Green,
     Blue,
@@ -24,10 +26,17 @@ fn parse_color(input: &str) -> IResult<&str, Color> {
     alt((red, green, blue))(input)
 }
 
-struct Subset {
-    red: u32,
-    green: u32,
-    blue: u32,
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub struct Subset {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+impl Subset {
+    fn power(&self) -> usize {
+        self.red * self.green * self.blue
+    }
 }
 
 fn parse_num(input: &str) -> IResult<&str, usize> {
@@ -41,19 +50,20 @@ fn parse_subset(input: &str) -> IResult<&str, Subset> {
         blue: 0,
     };
     let parse_cube = |input| separated_pair(parse_num, space1, parse_color)(input);
-    let (input, pairs): (&str, Vec<(_, _)>) = many1(parse_cube)(input)?;
-    for pair in &pairs {
-        let (count, color): (usize, Color) = pairs;
+    let (input, pairs): (&str, Vec<(_, _)>) = separated_list1(tag(", "), parse_cube)(input)?;
+    for pairs in &pairs {
+        let (count, color): &(usize, Color) = pairs;
         *(match color {
             Color::Red => &mut ret.red,
             Color::Blue => &mut ret.blue,
             Color::Green => &mut ret.green,
-        }) = count;
+        }) = *count;
     }
     Ok((input, ret))
 }
 
-struct Game {
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct Game {
     id: usize,
     subsets: Vec<Subset>,
 }
@@ -68,7 +78,15 @@ fn parse_game(input: &str) -> IResult<&str, Game> {
 impl ParseInput<'_, { Day::Day2 }> for AOC2023<{ Day::Day2 }> {
     type Parsed = Vec<Game>;
 
-    fn parse_input(&self, input: &'_ str) -> Result<Self::Parsed> {}
+    fn parse_input(&self, input: &'_ str) -> Result<Self::Parsed> {
+        input
+            .lines()
+            .map(|line| {
+                let (_, ret) = parse_game(line).map_err(|e| e.to_owned())?;
+                Ok(ret)
+            })
+            .collect()
+    }
 }
 
 fn is_possible(game: &[Subset], bag: &Subset) -> bool {
@@ -86,6 +104,16 @@ fn is_possible(game: &[Subset], bag: &Subset) -> bool {
     true
 }
 
+fn smallest_possible(subsets: &[Subset]) -> Subset {
+    let mut smallest = subsets[0].clone();
+    for subset in subsets {
+        smallest.red = max(smallest.red, subset.red);
+        smallest.blue = max(smallest.blue, subset.blue);
+        smallest.green = max(smallest.green, subset.green);
+    }
+    smallest
+}
+
 impl Solution<'_, { Day::Day2 }, { Part::One }> for AOC2023<{ Day::Day2 }> {
     type Input = Vec<Game>;
     type Output = usize;
@@ -101,5 +129,68 @@ impl Solution<'_, { Day::Day2 }, { Part::One }> for AOC2023<{ Day::Day2 }> {
             .filter(|game| is_possible(&game.subsets, &bag))
             .map(|game| game.id)
             .sum())
+    }
+}
+
+impl Solution<'_, { Day::Day2 }, { Part::Two }> for AOC2023<{ Day::Day2 }> {
+    type Input = Vec<Game>;
+    type Output = usize;
+
+    fn solve(&self, input: &Self::Input) -> Result<Self::Output> {
+        Ok(input
+            .iter()
+            .map(|game| smallest_possible(&game.subsets))
+            .map(|subset| subset.power())
+            .sum())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aoc_runner::PartOneVerifier;
+    use aoc_runner::PartTwoVerifier;
+
+    #[test]
+    fn test_parse_color() -> Result<()> {
+        assert_eq!(parse_color("green")?, ("", Color::Green));
+        assert_eq!(parse_color("red")?, ("", Color::Red));
+        assert_eq!(parse_color("blue")?, ("", Color::Blue));
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_subset() -> Result<()> {
+        assert_eq!(
+            parse_subset("1 green, 6 red, 4 blue")?,
+            (
+                "",
+                Subset {
+                    red: 6,
+                    blue: 4,
+                    green: 1
+                }
+            )
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_game() -> Result<()> {
+        assert_eq!(parse_game("Game 1: 1 green, 6 red, 4 blue; 2 blue, 6 green, 7 red; 3 red, 4 blue, 6 green; 3 green; 3 blue, 2 green, 1 red")?, 
+                   ("", Game {id: 1,subsets: [Subset { red: 6, green: 1, blue: 4}, Subset { red: 7, blue: 2, green: 6}, Subset {red: 3, blue: 4, green: 6}, Subset {red: 0, blue: 0, green: 3}, Subset {red: 1, blue: 3, green: 2}].to_vec()}));
+        Ok(())
+    }
+
+    #[test]
+    fn test() -> Result<()> {
+        let problem = super::AOC2023::<{ Day::Day2 }>;
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        problem.test_part1(&input, 8)?;
+        problem.test_part2(&input, 2286)
     }
 }
