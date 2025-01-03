cb31025e0b8ac8f4eddc369267fd05064d3e020a
Brennan Tracy <brennantracy4@gmail.com>
2025-01-03 15:50:36 -0800

    day 12 is too hard.

--- aoc-2024/src/day12.rs
+++ aoc-2024/src/day12.rs
@@ -8,17 +8,22 @@ use anyhow::Result;
 use aoc_runner::{point2d::Point2D, Day, ParseInput, Part, Solution};
 
 type Num = usize;
-type Point = Point2D<usize>;
+type Point = Point2D<i32>;
 
 pub struct Region {
     perimeter: Num,
+    sides: Num,
     plots: HashSet<Point>,
-    plant: char,
+    _plant: char,
 }
 
 impl Region {
     fn cost(&self) -> Num {
         return self.plots.len() * self.perimeter;
+    }
+
+    fn cost2(&self) -> Num {
+        return self.plots.len() * self.sides;
     }
 }
 
@@ -31,60 +36,62 @@ pub struct Garden {
 impl Garden {
     fn regions(&self) -> Vec<Region> {
         let get_neighbors = |point: Point| {
-            let ret = Vec::new();
-            if point.x != 0 {
-                ret.push(Point {
+            vec![
+                Point {
                     x: point.x - 1,
                     ..point
-                });
-            }
-            if point.x != self.width - 1 {
-                ret.push(Point {
+                },
+                Point {
                     x: point.x + 1,
                     ..point
-                });
-            }
-            if point.y != 0 {
-                ret.push(Point {
+                },
+                Point {
                     y: point.y - 1,
                     ..point
-                });
-            }
-            if point.y != self.width - 1 {
-                ret.push(Point {
+                },
+                Point {
                     y: point.y + 1,
                     ..point
-                });
-            }
-            ret
+                },
+            ]
+        };
+        let inbounds = |Point2D { x, y }: Point| {
+            x >= 0 && x < self.width as i32 && y >= 0 && y < self.height as i32
         };
         let mut visited = HashSet::new();
         let mut regions = Vec::new();
-        let dfs = |point: Point, target: char| -> Region {
+        let dfs = |point: Point, target: char, visited: &mut HashSet<Point>| -> Region {
+            let mut perimeter = 0;
             let mut to_visit = Vec::from([point]);
             let mut plots = HashSet::new();
             while let Some(point) = to_visit.pop() {
                 if visited.contains(&point) {
                     continue;
                 }
-                if self.plots[&point] != target {
-                    continue;
-                }
                 visited.insert(point);
                 plots.insert(point);
                 for neighbor in get_neighbors(point) {
-                    to_visit.push(neighbor);
+                    if inbounds(neighbor) {
+                        if self.plots[&neighbor] != target {
+                            perimeter += 1;
+                        } else {
+                            to_visit.push(neighbor);
+                        }
+                    } else {
+                        perimeter += 1;
+                    }
                 }
             }
             Region {
                 perimeter,
+                sides,
                 plots,
-                plant: target,
+                _plant: target,
             }
         };
-        for (point, plant) in self.plots {
-            if !visited.contains(&point) {
-                regions.push(dfs(point, self.plots[&point]));
+        for point in self.plots.keys() {
+            if !visited.contains(point) {
+                regions.push(dfs(*point, self.plots[&point], &mut visited));
             }
         }
         regions
@@ -92,6 +99,10 @@ impl Garden {
 
     fn price(&self) -> Num {
         self.regions().iter().map(|region| region.cost()).sum()
+    }
+
+    fn price2(&self) -> Num {
+        self.regions().iter().map(|region| region.cost2()).sum()
     }
 }
 
@@ -108,7 +119,15 @@ impl FromStr for Garden {
             .map(|(y, row)| {
                 row.into_iter()
                     .enumerate()
-                    .map(|(x, c)| (Point { x, y }, c))
+                    .map(|(x, c)| {
+                        (
+                            Point {
+                                x: x as i32,
+                                y: y as i32,
+                            },
+                            c,
+                        )
+                    })
                     .collect::<HashMap<_, _>>()
             })
             .flatten()
