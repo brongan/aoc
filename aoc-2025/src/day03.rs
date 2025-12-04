edf7dfcfedac0159eab82fdb39b4e1f7efafa20e
Brennan Tracy <brennantracy4@gmail.com>
2025-12-04 00:14:10 -0800

    2025 Day 3 complete.

--- aoc-2025/src/day03.rs
+++ aoc-2025/src/day03.rs
@@ -1,6 +1,7 @@
 use super::AOC2025;
 use anyhow::Result;
 use aoc_runner::{Day, ParseInput, Part, Solution};
+use rayon::prelude::*;
 
 impl ParseInput<'_, { Day::Day3 }> for AOC2025<{ Day::Day3 }> {
     type Parsed = Vec<Vec<u64>>;
@@ -37,17 +38,24 @@ impl Solution<'_, { Day::Day3 }, { Part::One }> for AOC2025<{ Day::Day3 }> {
     }
 }
 
+fn joltage2(slice: &[u64], count: u32) -> u64 {
+    if count == 0 || slice.len() == 0 {
+        return 0;
+    }
+    let (index, max) = &slice[..slice.len() - count as usize + 1]
+        .iter()
+        .enumerate()
+        .max_by(|(i, a), (j, b)| a.cmp(b).then(i.cmp(j).reverse()))
+        .unwrap();
+    let val = *max * 10_u64.pow(count - 1);
+    val + joltage2(&slice[index + 1..], count - 1)
+}
+
 impl Solution<'_, { Day::Day3 }, { Part::Two }> for AOC2025<{ Day::Day3 }> {
     type Input = Vec<Vec<u64>>;
     type Output = u64;
 
     fn solve(&self, input: &Self::Input) -> Result<Self::Output> {
-        let joltage = |line: &[u64]| -> u64 {
-            line.array_windows::<12>()
-                .map(|window| window.iter().fold(0, |acc, num| acc * 10 + num))
-                .max()
-                .unwrap()
-        };
-        Ok(input.iter().map(|line| joltage(line)).sum())
+        Ok(input.par_iter().map(|line| joltage2(line, 12)).sum())
     }
 }
